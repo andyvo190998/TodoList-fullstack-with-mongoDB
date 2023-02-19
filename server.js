b5f10513da74e5c166de37f7f0fa703653ad15b7
Andy Vo <andyvo@Andys-MacBook-Pro.local>
2023-02-19 15:32:35 +0100

    add build folder

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ require("dotenv").config();
 
 
 //congifig
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000", "http://todo-list.onrender.com"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 require('dotenv').config();
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
